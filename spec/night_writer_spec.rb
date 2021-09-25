e9fc614700ea4ec39f1c4ef125dce8604d7baef1
craig cardwell <wizeduvcc@gmail.com>
2021-09-25 14:04:19 -0600

    Initial commit

---
+++ spec/night_writer_spec.rb
@@ -0,0 +1,10 @@
+# require './lib/message.txt'
+# # require './lib/braille.txt'
+# require './lib/night_writer.rb'
+#
+# RSpec.describe Converter do
+#   it 'exists' do
+#     conv = Converter.new
+#     expect(conv).to be_an_instance_of(Converter)
+#   end
+# end
