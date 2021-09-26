7a3afa3e431daaa45951e4b46e82ee686ee38c1b
craig cardwell <wizeduvcc@gmail.com>
2021-09-25 20:04:00 -0600

    feat explore options to create command line interface

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,25 @@
+# require './message.txt'
 
+# class File
+#   def initialize()
+#   end
+# end
+
+
+# file_name = "braille.txt"
+#
+#
+# # puts "Created '#{file_name}' containing 256 characters"
+# # puts "Please input a file name in the following format: braille.txt"
+#
+# file_name = gets.chomp
+#
+# puts "Created '#{file_name}' containing 256 characters"
+
+
+
+
+# class Converter
+#   def initialize()
+#   end
+# end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,10 +1,15 @@
-# require './lib/message.txt'
-# # require './lib/braille.txt'
-# require './lib/night_writer.rb'
-#
-# RSpec.describe Converter do
-#   it 'exists' do
-#     conv = Converter.new
-#     expect(conv).to be_an_instance_of(Converter)
-#   end
-# end
+# require './message.txt'
+# require './lib/braille.txt'
+require './lib/night_writer.rb'
+
+RSpec.describe File do
+  it 'exists' do
+    iter = File.open("message.txt").read
+    expect(iter).to eq("Created 'braille.txt' containing 256 characters")
+  end
+
+  # it 'can open txt file' do
+  #   conv = File.new
+  #
+  # end
+end
