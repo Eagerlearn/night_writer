d2fcfe78e2d79efca4ec6fe47c28fad88875703f
craig cardwell <wizeduvcc@gmail.com>
2021-09-27 14:18:34 -0600

    feat: add NightWriter class, and read, convert, and write methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,19 +1,64 @@
+class NightWriter
+  attr_reader :input,
+              :output
+  def initialize
+    @input  = ARGV[0]
+    @output = ARGV[1]
+    # require "pry"; binding.pry
+  end
+
+  def read
+    file = File.open(input).read
+  end
+
+  def convert
+    print
+  end
+
+  def write
+    # message = "Created 'braille.txt' containing 256 characters"
+    File.open(output, "w") { |f| f.write(convert)}
+    puts "Created '#{output}' containing '#{read.size}' characters"
+    # require "pry"; binding.pry
+  end
+end
+NightWriter.new.write
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/usr/bin/env ruby
 
-
-require 'optparse'
-
-options = {}
-
-OptionParser.new do |op|
- op.on("-e", "--email", "Your email address") do |value|
-    options[:name] = value
-  end
-end.parse!
-
-
-
-puts 'Your email address is:' + options[:name]
+#
+# require 'optparse'
+#
+# options = {}
+#
+# OptionParser.new do |op|
+#  op.on("-e", "--email", "Your email address") do |value|
+#     options[:name] = value
+#   end
+# end.parse!
+#
+#
+#
+# puts 'Your email address is:' + options[:name]
 
 # puts "Created 'braille.txt' containing 256 characters"
 
